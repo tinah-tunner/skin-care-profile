49d10a7a2d0d2269fb0bb11dedad323de95b2719
Carmina Tinah Mbatsane <mbatsicarmina@gmail.com>
2026-05-04 09:42:49 +0200

    Initial commit

---
+++ src/main/java/com/tinah_tunner/skin/care/profile/SkinCareProfileApplication.java
@@ -0,0 +1,13 @@
+package com.tinah_tunner.skin.care.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinCareProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinCareProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinah_tunner/skin/care/profile/SkinCareProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinah_tunner.skin.care.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinCareProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
